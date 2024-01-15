3af8793783e4a6148f015ff6f3b2b1683f6ae19f
pausm99 <pausm99@gmail.com>
2024-01-15 18:20:56 +0100

    ADD: list component

---
+++ src/app/components/reservation/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+  public stars = [1, 2, 3, 4, 5]
+
+
+}
