77963196ecfb82bf5a24325f7feada2aa3e4626e
pausm99 <pausm99@gmail.com>
2024-01-15 18:24:17 +0100

    FIX: get api url from env

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../../interfaces/user';
+import { environment } from '../../../environments/environment';
+
+const API_URL = environment.api_url;
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +14,14 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   register(user: User): Observable<any> {
-    return this.http.post<User>('http://localhost:3000/user/register', user);
+    return this.http.post<User>(`${API_URL}/user/register`, user);
   }
 
   getUserByEmail(email: string): Observable<any> {
-    return this.http.get<any>(`http://localhost:3000/user/${email}`);
+    return this.http.get<any>(`${API_URL}/user/${email}`);
   }
 
   login(body: any): Observable<any> {
-    return this.http.post<any>(`http://localhost:3000/user/login`, body);
+    return this.http.post<any>(`${API_URL}/user/login`, body);
   }
 }
