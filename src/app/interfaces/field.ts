fb3fb36f0c5e97bb8cc40677f5a3d2556a68c837
pausm99 <pausm99@gmail.com>
2024-01-15 21:52:15 +0100

    ADD: added name field

--- src/app/interfaces/field.ts
+++ src/app/interfaces/field.ts
@@ -1,11 +1,12 @@
 export interface Field {
   id: number
   company_id: number;
+  name: string;
   type: 'FutSala' | 'Fut5' | 'Fut7' | 'Fut11';
   location_lat: number;
   location_lng: number;
   address : string;
-  is_public : boolean;
+  public : boolean;
   width : number;
   length : number;
   opening_time : string;
