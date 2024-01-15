7c4df9f4116a961e9acb61395b4426364abf0171
pausm99 <pausm99@gmail.com>
2024-01-15 21:51:34 +0100

    1st version of component

--- src/app/components/reservation/list/list.component.ts
+++ src/app/components/reservation/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FieldService } from '../../../services/field.service';
+import { Field } from '../../../interfaces/field';
+
+const maxStars: number = 5;
 
 @Component({
   selector: 'app-list',
@@ -8,7 +12,13 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-  public stars = [1, 2, 3, 4, 5]
 
+  public fields: Field[] = [];
+  public stars: any[] = new Array(maxStars);
 
+  constructor(private fieldService: FieldService) {
+    this.fieldService.getAllFields().subscribe((res) => {
+      this.fields = res;
+    });
+  }
 }
