1dbd5ea17eb1eb486bf5a2e3c086a9d179ed4300
pausm99 <pausm99@gmail.com>
2024-01-15 21:52:31 +0100

    1st version of component

--- src/app/components/reservation/reservation.component.ts
+++ src/app/components/reservation/reservation.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { NavbarService } from '../../services/navbar/navbar.service';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-reservation',
   standalone: true,
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.scss'
 })
 export class ReservationComponent {
+
+  public paddingTop!: string;
+  @ViewChild('reservationSection') reservationSection?: ElementRef;
+
+  constructor(private navbarService: NavbarService, private cd: ChangeDetectorRef) {}
+
+  ngAfterViewChecked(): void {
+    let heroElement = this.reservationSection?.nativeElement;
+    const styles = window.getComputedStyle(heroElement);
+    const paddingBottom = Number(styles.paddingBottom.split('px')[0]);
+    const paddingTop = paddingBottom + this.navbarService.getHeight();
+    this.paddingTop = paddingTop + 'px !important';
+    this.cd.detectChanges();
+  }
 
 }
