2cd50e5d961852413c3ce4be11b21068db08746a
pausm99 <pausm99@gmail.com>
2024-01-15 20:38:02 +0100

    ADD: field service

---
+++ src/app/services/field.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Field } from '../interfaces/field';
+
+const API_URL = environment.api_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FieldService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllFields(): Observable<Field[]> {
+    return this.http.get<Field[]>(`${API_URL}/fields`);
+  }
+}
