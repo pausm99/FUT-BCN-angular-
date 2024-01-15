e471144ba1f09534207657a7500365f5814c2ca5
pausm99 <pausm99@gmail.com>
2024-01-15 21:00:46 +0100

    FIX: deleted unnecessary console.log

--- src/app/interceptors/request.interceptor.ts
+++ src/app/interceptors/request.interceptor.ts
@@ -9,7 +9,6 @@ export const authRequestInterceptor: HttpInterceptorFn = (req, next) => {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
-    console.log(authReq);
     return next(authReq);
   }
   return next(req);
