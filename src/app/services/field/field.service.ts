d6590fe490e6acd4c9dc203c023e2ae2ecb05cbd
pausm99 <pausm99@gmail.com>
2024-01-16 11:25:16 +0100

    changed field service location

--- src/app/services/field/field.service.ts
+++ src/app/services/field/field.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-import { Field } from '../interfaces/field';
+import { environment } from '../../../environments/environment';
+import { Field } from '../../interfaces/field';
 
 const API_URL = environment.api_url;
 
