36cd811ff757ae52ab74c580cf70b53df095df1f
pausm99 <pausm99@gmail.com>
2024-02-16 13:33:36 +0100

    FIX: links to home

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,10 +1,11 @@
+import { RouterLink } from '@angular/router';
 import { TitleService } from './../../../services/title/title.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
