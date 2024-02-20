53f1a0f82512db43ced6be6ebf27536b367678ed
pausm99 <pausm99@gmail.com>
2024-02-20 15:39:10 +0100

    Added warning message when company profile visits reservations route

--- src/app/guards/player/player.guard.ts
+++ src/app/guards/player/player.guard.ts
@@ -1,16 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../../services/user/user.service';
+import { ToastService } from '../../services/toast/toast.service';
 
 export const PlayerGuard: CanActivateFn = (route, state) => {
 
   const router = inject(Router);
   const userService = inject(UserService);
+  const toastService = inject(ToastService)
 
   const role = userService.userInfo().role;
 
   if (role !== 'player') {
     router.navigate(['/home']);
+    toastService.showDanger(`Company profiles aren't allowed to book fields`)
     return false;
   }
 
